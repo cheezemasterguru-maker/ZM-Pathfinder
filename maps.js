3b3927c0e21ca119e66cd1c6d2538732764b00b2
CheezeMasterGuru <cheezemasterguru@gmail.com>
2026-03-25 00:04:55 -0400

    Update maps.js

--- maps.js
+++ maps.js
@@ -2,12 +2,12 @@ window.ZM_MAP_LIBRARY = {
   Main: {
     "Essence Cave": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
     "Treasure Trove of Gems": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
-    "Grand Canyon": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
-    "Sweet Valley": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
-    "Treasures in Ice": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
-    "Moon Odyssey": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
-    "Volcano Awakening": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
-    "Excavations in the Sand": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"]
+    "Grand Canyon": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+    "Sweet Valley": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+    "Treasures in Ice": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+    "Moon Odyssey": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+    "Volcano Awakening": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+    "Excavations in the Sand": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
   },
 
   Legacy: {
@@ -105,8 +105,66 @@ window.ZM_MAP_DATA = {
           ["S", "S", 2, "", 2, 4, 3],
           [1, 2, "", "", "", 3, ""]
         ]
-      }
+      },
+
+      "Chamber 2": {
+        title: "Treasures in Ice - Chamber 2",
+        gateType: "standard",
+        grid: [
+          [21, 24, 23, 21, 18, 17, ""],
+          [20, 22, 23, 24, "X", "S", "S"],
+          [19, 21, 20, 22, 17, "S", "S"],
+          [18, 22, "B", 18, 21, "S", "S"],
+          ["X", "X", 12, 13, 17, 14, 18],
+          ["S", "S", "X", 15, 10, "X", 12],
+          ["S", "S", 13, 11, "B", 11, 16],
+          ["S", "S", 9, 12, 10, 13, "X"],
+          [8, 9, 10, 9, 8, "S", "S"],
+          [7, 8, 8, 7, "X", "S", "S"],
+          ["", 7, "", "", 7, "S", "S"]
+        ]
+      },
+
+      "Chamber 3": {
+        title: "Treasures in Ice - Chamber 3",
+        gateType: "standard",
+        grid: [
+          ["X", 30, 31, 27, 29, 31, 26],
+          ["S", "S", 28, 32, 31, 30, 28],
+          ["S", "S", "X", 29, 28, 28, "X"],
+          ["S", "S", 23, 30, 27, "S", "S"],
+          [22, 23, 28, 22, 24, "S", "S"],
+          [25, "B", 21, 27, 20, "S", "S"],
+          [20, 26, "X", 25, 23, 19, 26],
+          [24, "X", 23, 21, 26, 22, "X"],
+          ["X", 21, 20, 19, "B", "S", "S"],
+          [19, 16, 18, 20, 17, "S", "S"],
+          [17, 15, 21, 16, "X", "S", "S"]
+        ]
+      },
+
+      "Chamber 4": {
+        title: "Treasures in Ice - Chamber 4",
+        gateType: "end",
+        grid: [
+          [42, 32, 37, 41, 43, 32, 46],
+          [40, 31, 44, "X", 33, 42, 31],
+          [43, 33, "X", 32, 40, 43, 42],
+          [31, 42, 34, 37, 35, 41, 30],
+          [38, 32, 39, 40, 38, "X", "B"],
+          [33, "X", 30, 34, 36, "X", 42],
+          [37, 31, 38, 37, 32, 31, "X"],
+          [29, 30, 36, 35, 33, "S", "S"],
+          ["X", 32, 29, 31, 30, "S", "S"],
+          ["X", "X", "X", 32, "B", "S", "S"],
+          ["X", 33, 26, 27, 28, "X", "X"],
+          ["X", 26, 32, 26, "X", "X", "X"]
+        ]
+      },
+
+      "Graveyard": null
     }
   },
+
   Legacy: {}
 };
