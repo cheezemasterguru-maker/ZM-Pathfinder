f3c60394b100f7a31c36e4a3d3646dfdb695a213
CheezeMasterGuru <cheezemasterguru@gmail.com>
2026-03-25 15:16:45 -0400

    Update map-library.js

--- map-library.js
+++ map-library.js
@@ -1,1 +1,87 @@
+window.ZM_MAP_LIBRARY = {
+  Main: {
+    "Essence Cave": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
+    "Treasure Trove of Gems": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
+    "Grand Canyon": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+    "Sweet Valley": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+    "Treasures in Ice": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+    "Moon Odyssey": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+    "Volcano Awakening": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+    "Excavations in the Sand": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+  },
 
+  Legacy: {
+    "Love Story": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Clover Festival": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Easter Egg Hunt": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "4th of July": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Mystery Reef": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Teamwork Festival": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Halloween": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Fall Festival": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Winter Break": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    }
+  }
+};
