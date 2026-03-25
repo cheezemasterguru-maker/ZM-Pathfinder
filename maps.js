12a1b97d351504433399a782f4ed24d535c5791c
CheezeMasterGuru <cheezemasterguru@gmail.com>
2026-03-24 23:32:54 -0400

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,133 +1,112 @@
-window.ZM_MAPS = {
+window.ZM_MAP_LIBRARY = {
   Main: {
-    "Essence Cave": {
-      "Chamber 1": null,
-      "Chamber 2": null,
-      "Chamber 3": null,
-      "Graveyard": null
-    },
-
-    "Treasure Trove of Gems": {
-      "Chamber 1": null,
-      "Chamber 2": null,
-      "Chamber 3": null,
-      "Graveyard": null
-    },
-
-    "Grand Canyon": {
-      "Chamber 1": null,
-      "Chamber 2": null,
-      "Chamber 3": null,
-      "Graveyard": null
-    },
-
-    "Sweet Valley": {
-      "Chamber 1": null,
-      "Chamber 2": null,
-      "Chamber 3": null,
-      "Graveyard": null
-    },
-
-    "Treasures in Ice": {
-      "Chamber 1": {
-        title: "Treasures in Ice - Chamber 1",
-        type: "standard",
-        isGraveyard: false,
-        grid: [
-          [8, 9, 11, 9, 12, 8, 9],
-          [10, "block", 8, "block", 7, 11, 7],
-          [8, 7, 9, 6, 9, 6, "block"],
-          ["block", 8, 6, 8, "bubble", "shaft", ""],
-          ["block", 6, 7, 6, 5, "", ""],
-          ["block", 5, 4, 5, "block", "", ""],
-          ["block", 4, 5, 4, 5, 4, 6],
-          ["shaft", "", 4, 5, 4, "block", 5],
-          ["", "", 3, 4, 3, 5, 4],
-          ["", "", 2, "", 2, 4, 3],
-          [1, 2, "", "", "", 3, ""]
-        ]
-      },
-      "Chamber 2": null,
-      "Chamber 3": null,
-      "Graveyard": null
-    },
-
-    "Moon Odyssey": {
-      "Chamber 1": null,
-      "Chamber 2": null,
-      "Chamber 3": null,
-      "Graveyard": null
-    },
-
-    "Volcano Awakening": {
-      "Chamber 1": null,
-      "Chamber 2": null,
-      "Chamber 3": null,
-      "Graveyard": null
-    },
-
-    "Excavations in the Sand": {
-      "Chamber 1": null,
-      "Chamber 2": null,
-      "Chamber 3": null,
-      "Graveyard": null
-    }
+    "Essence Cave": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
+    "Treasure Trove of Gems": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
+    "Grand Canyon": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
+    "Sweet Valley": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
+    "Treasures in Ice": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
+    "Moon Odyssey": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
+    "Volcano Awakening": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"],
+    "Excavations in the Sand": ["Chamber 1", "Chamber 2", "Chamber 3", "Graveyard"]
   },
 
   Legacy: {
-    "Love Story": buildLegacyStructure(),
-    "Clover Festival": buildLegacyStructure(),
-    "Easter Egg Hunt": buildLegacyStructure(),
-    "4th of July": buildLegacyStructure(),
-    "Mystery Reef": buildLegacyStructure(),
-    "Teamwork Festival": buildLegacyStructure(),
-    "Halloween": buildLegacyStructure(),
-    "Fall Festival": buildLegacyStructure(),
-    "Winter Break": buildLegacyStructure()
+    "Love Story": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Clover Festival": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Easter Egg Hunt": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "4th of July": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Mystery Reef": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Teamwork Festival": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Halloween": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Fall Festival": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    },
+    "Winter Break": {
+      "Mine 1": ["Chamber 1", "Chamber 2"],
+      "Mine 2": ["Chamber 1", "Chamber 2", "Chamber 3"],
+      "Mine 3": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 4": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4"],
+      "Mine 5": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"],
+      "Deep Mine": ["Chamber 1", "Chamber 2", "Chamber 3", "Chamber 4", "Graveyard"]
+    }
   }
 };
 
-function buildLegacyStructure() {
-  return {
-    "Mine 1": {
-      "Chamber 1": null,
-      "Chamber 2": null
-    },
-
-    "Mine 2": {
-      "Chamber 1": null,
-      "Chamber 2": null,
-      "Chamber 3": null
-    },
-
-    "Mine 3": {
-      "Chamber 1": null,
-      "Chamber 2": null,
-      "Chamber 3": null,
-      "Chamber 4": null
-    },
-
-    "Mine 4": {
-      "Chamber 1": null,
-      "Chamber 2": null,
-      "Chamber 3": null,
-      "Chamber 4": null
-    },
-
-    "Mine 5": {
-      "Chamber 1": null,
-      "Chamber 2": null,
-      "Chamber 3": null,
-      "Chamber 4": null,
-      "Graveyard": null
-    },
-
-    "Deep Mine": {
-      "Chamber 1": null,
-      "Chamber 2": null,
-      "Chamber 3": null,
-      "Chamber 4": null,
-      "Graveyard": null
+window.ZM_MAP_DATA = {
+  Main: {
+    "Treasures in Ice": {
+      "Chamber 1": {
+        title: "Treasures in Ice - Chamber 1",
+        gateType: "standard",
+        grid: [
+          [8, 9, 11, 9, 12, 8, 9],
+          [10, "X", 8, "X", 7, 11, 7],
+          [8, 7, 9, 6, 9, 6, "X"],
+          ["X", 8, 6, 8, "B", "S", "S"],
+          ["X", 6, 7, 6, 5, "S", "S"],
+          ["X", 5, 4, 5, "X", "S", "S"],
+          ["X", 4, 5, 4, 5, 4, 6],
+          ["S", "S", 4, 5, 4, "X", 5],
+          ["S", "S", 3, 4, 3, 5, 4],
+          ["S", "S", 2, "", 2, 4, 3],
+          [1, 2, "", "", "", 3, ""]
+        ]
+      }
     }
-  };
-}
+  },
+  Legacy: {}
+};
