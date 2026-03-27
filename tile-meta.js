782d28dd0d736508284b8437d78974803d816177
CheezeMasterGuru <cheezemasterguru@gmail.com>
2026-03-27 03:22:03 -0400

    Update tile-meta.js

--- tile-meta.js
+++ tile-meta.js
@@ -1,1 +1,67 @@
+window.ZM_TILE_META = {
+  Main: {
+    "Treasures in Ice": {
+      "Chamber 1": { tiles: {} },
+      "Chamber 2": { tiles: {} },
+      "Chamber 3": { tiles: {} },
+      "Chamber 4": { tiles: {} },
+      "Graveyard": { tiles: {} }
+    },
 
+    "Excavations in the Sand": {
+      "Chamber 1": { tiles: {} },
+      "Chamber 2": { tiles: {} },
+      "Chamber 3": { tiles: {} },
+      "Chamber 4": { tiles: {} },
+      "Graveyard": { tiles: {} }
+    },
+
+    "Sweet Valley": {
+      "Chamber 1": { tiles: {} },
+      "Chamber 2": { tiles: {} },
+      "Chamber 3": { tiles: {} },
+      "Chamber 4": { tiles: {} },
+      "Graveyard": { tiles: {} }
+    },
+
+    "Moon Odyssey": {
+      "Chamber 1": { tiles: {} },
+      "Chamber 2": { tiles: {} },
+      "Chamber 3": { tiles: {} },
+      "Chamber 4": { tiles: {} },
+      "Graveyard": { tiles: {} }
+    },
+
+    "Volcano Awakening": {
+      "Chamber 1": { tiles: {} },
+      "Chamber 2": { tiles: {} },
+      "Chamber 3": { tiles: {} },
+      "Chamber 4": { tiles: {} },
+      "Graveyard": { tiles: {} }
+    },
+
+    "Grand Canyon": {
+      "Chamber 1": { tiles: {} },
+      "Chamber 2": { tiles: {} },
+      "Chamber 3": { tiles: {} },
+      "Chamber 4": { tiles: {} },
+      "Graveyard": { tiles: {} }
+    },
+
+    "Essence Cave": {
+      "Chamber 1": { tiles: {} },
+      "Chamber 2": { tiles: {} },
+      "Chamber 3": { tiles: {} },
+      "Graveyard": { tiles: {} }
+    },
+
+    "Treasure Trove of Gems": {
+      "Chamber 1": { tiles: {} },
+      "Chamber 2": { tiles: {} },
+      "Chamber 3": { tiles: {} },
+      "Graveyard": { tiles: {} }
+    }
+  },
+
+  Legacy: {}
+};
