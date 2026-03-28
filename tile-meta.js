39f6e9db90943841a30ae7281b0eeec58020473e
CheezeMasterGuru <cheezemasterguru@gmail.com>
2026-03-28 01:58:32 -0400

    Update tile-meta.js

--- tile-meta.js
+++ tile-meta.js
@@ -2,28 +2,28 @@ window.ZM_TILE_META = {
   Main: {
     "Treasures in Ice": {
       "Chamber 1": {
-  tiles: {
-    "0,0": { object: "keys" },
-    "0,2": { object: "chest", subtype: "iron" },
-    "0,4": { object: "emblems" },
-    "0,6": { object: "chest", subtype: "wood" },
-    "1,0": { object: "emblems" },
-    "1,5": { object: "essence" },
-    "2,0": { object: "chest", subtype: "wood" },
-    "2,4": { object: "keys" },
-    "3,1": { object: "essence" },
-    "3,3": { object: "emblems" },
-    "4,3": { object: "chest", subtype: "iron" },
-    "5,1": { object: "essence" },
-    "6,1": { object: "chest", subtype: "wood" },
-    "6,6": { object: "keys" },
-    "8,3": { object: "keys" },
-    "8,5": { object: "essence" },
-    "9,5": { object: "chest", subtype: "wood" },
-    "10,1": { object: "emblems" }
-  }
-}
-},
+        tiles: {
+          "0,0": { object: "keys" },
+          "0,2": { object: "chest", subtype: "iron" },
+          "0,4": { object: "emblems" },
+          "0,6": { object: "chest", subtype: "wood" },
+          "1,0": { object: "emblems" },
+          "1,5": { object: "essence" },
+          "2,0": { object: "chest", subtype: "wood" },
+          "2,4": { object: "keys" },
+          "3,1": { object: "essence" },
+          "3,3": { object: "emblems" },
+          "4,3": { object: "chest", subtype: "iron" },
+          "5,1": { object: "essence" },
+          "6,1": { object: "chest", subtype: "wood" },
+          "6,6": { object: "keys" },
+          "8,3": { object: "keys" },
+          "8,5": { object: "essence" },
+          "9,5": { object: "chest", subtype: "wood" },
+          "10,1": { object: "emblems" }
+        }
+      },
+
       "Chamber 2": { tiles: {} },
       "Chamber 3": { tiles: {} },
       "Chamber 4": { tiles: {} },
