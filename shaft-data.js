eb40139e1ebe779c56a8145ba72683a859ebfa50
CheezeMasterGuru <cheezemasterguru@gmail.com>
2026-03-30 18:34:54 -0400

    Create shaft-data.js

---
+++ shaft-data.js
@@ -0,0 +1,191 @@
+window.ZM_SHAFT_DATA = {
+  MainDeep: {
+    "DEEP 1": {
+      "Chamber 1": [],
+      "Chamber 2": [],
+      "Chamber 3": []
+    },
+    "DEEP 2": {
+      "Chamber 1": [],
+      "Chamber 2": [],
+      "Chamber 3": [],
+      "Chamber 4": []
+    },
+    "DEEP 3": {
+      "Chamber 1": [],
+      "Chamber 2": [],
+      "Chamber 3": [],
+      "Chamber 4": []
+    },
+    "DEEP 4": {
+      "Chamber 1": [],
+      "Chamber 2": [],
+      "Chamber 3": []
+    }
+  },
+
+  Main: {
+    "Treasures in Ice": {
+      "Chamber 1": [
+        { shaftType: "Beryl", level: null, auto: null },
+        { shaftType: "Quartz", level: null, auto: null }
+      ],
+      "Chamber 2": [
+        { shaftType: "Cobalt", level: null, auto: null },
+        { shaftType: "Amber", level: null, auto: null },
+        { shaftType: "Ametrine", level: null, auto: null }
+      ],
+      "Chamber 3": [
+        { shaftType: "Barite", level: null, auto: null },
+        { shaftType: "Elendir", level: null, auto: null },
+        { shaftType: "Lapis Lazuli", level: null, auto: null }
+      ],
+      "Chamber 4": [
+        { shaftType: "Aventurine", level: null, auto: null }
+      ],
+      "Graveyard": []
+    },
+
+    "Excavations in the Sand": {
+      "Chamber 1": [
+        { shaftType: "Beryl", level: null, auto: null },
+        { shaftType: "Quartz", level: null, auto: null },
+        { shaftType: "Cobalt", level: null, auto: null }
+      ],
+      "Chamber 2": [
+        { shaftType: "Amber", level: null, auto: null },
+        { shaftType: "Ametrine", level: null, auto: null },
+        { shaftType: "Barite", level: null, auto: null }
+      ],
+      "Chamber 3": [
+        { shaftType: "Elendir", level: null, auto: null },
+        { shaftType: "Lapis Lazuli", level: null, auto: null },
+        { shaftType: "Aventurine", level: null, auto: null }
+      ],
+      "Chamber 4": [
+        { shaftType: "Obsidian", level: null, auto: null }
+      ],
+      "Graveyard": []
+    },
+
+    "Sweet Valley": {
+      "Chamber 1": [
+        { shaftType: "Beryl", level: null, auto: null },
+        { shaftType: "Quartz", level: null, auto: null },
+        { shaftType: "Cobalt", level: null, auto: null }
+      ],
+      "Chamber 2": [
+        { shaftType: "Amber", level: null, auto: null },
+        { shaftType: "Ametrine", level: null, auto: null },
+        { shaftType: "Barite", level: null, auto: null }
+      ],
+      "Chamber 3": [
+        { shaftType: "Elendir", level: null, auto: null },
+        { shaftType: "Lapis Lazuli", level: null, auto: null },
+        { shaftType: "Aventurine", level: null, auto: null }
+      ],
+      "Chamber 4": [
+        { shaftType: "Obsidian", level: null, auto: null }
+      ],
+      "Graveyard": []
+    },
+
+    "Moon Odyssey": {
+      "Chamber 1": [
+        { shaftType: "Beryl", level: null, auto: null },
+        { shaftType: "Quartz", level: null, auto: null },
+        { shaftType: "Cobalt", level: null, auto: null }
+      ],
+      "Chamber 2": [
+        { shaftType: "Amber", level: null, auto: null },
+        { shaftType: "Ametrine", level: null, auto: null },
+        { shaftType: "Barite", level: null, auto: null }
+      ],
+      "Chamber 3": [
+        { shaftType: "Elendir", level: null, auto: null },
+        { shaftType: "Lapis Lazuli", level: null, auto: null },
+        { shaftType: "Aventurine", level: null, auto: null }
+      ],
+      "Chamber 4": [
+        { shaftType: "Obsidian", level: null, auto: null }
+      ],
+      "Graveyard": []
+    },
+
+    "Volcano Awakening": {
+      "Chamber 1": [
+        { shaftType: "Beryl", level: null, auto: null },
+        { shaftType: "Quartz", level: null, auto: null }
+      ],
+      "Chamber 2": [
+        { shaftType: "Cobalt", level: null, auto: null },
+        { shaftType: "Amber", level: null, auto: null },
+        { shaftType: "Ametrine", level: null, auto: null }
+      ],
+      "Chamber 3": [
+        { shaftType: "Barite", level: null, auto: null },
+        { shaftType: "Elendir", level: null, auto: null },
+        { shaftType: "Lapis Lazuli", level: null, auto: null }
+      ],
+      "Chamber 4": [
+        { shaftType: "Aventurine", level: null, auto: null }
+      ],
+      "Graveyard": []
+    },
+
+    "Grand Canyon": {
+      "Chamber 1": [
+        { shaftType: "Beryl", level: null, auto: null },
+        { shaftType: "Quartz", level: null, auto: null }
+      ],
+      "Chamber 2": [
+        { shaftType: "Cobalt", level: null, auto: null },
+        { shaftType: "Amber", level: null, auto: null },
+        { shaftType: "Ametrine", level: null, auto: null }
+      ],
+      "Chamber 3": [
+        { shaftType: "Barite", level: null, auto: null },
+        { shaftType: "Elendir", level: null, auto: null },
+        { shaftType: "Lapis Lazuli", level: null, auto: null }
+      ],
+      "Chamber 4": [
+        { shaftType: "Aventurine", level: null, auto: null }
+      ],
+      "Graveyard": []
+    },
+
+    "Essence Cave": {
+      "Chamber 1": [
+        { shaftType: "Beryl", level: null, auto: null },
+        { shaftType: "Quartz", level: null, auto: null }
+      ],
+      "Chamber 2": [
+        { shaftType: "Cobalt", level: null, auto: null },
+        { shaftType: "Amber", level: null, auto: null },
+        { shaftType: "Ametrine", level: null, auto: null }
+      ],
+      "Chamber 3": [
+        { shaftType: "Barite", level: null, auto: null }
+      ],
+      "Graveyard": []
+    },
+
+    "Treasure Trove of Gems": {
+      "Chamber 1": [
+        { shaftType: "Beryl", level: null, auto: null },
+        { shaftType: "Quartz", level: null, auto: null }
+      ],
+      "Chamber 2": [
+        { shaftType: "Cobalt", level: null, auto: null },
+        { shaftType: "Amber", level: null, auto: null }
+      ],
+      "Chamber 3": [
+        { shaftType: "Ametrine", level: null, auto: null },
+        { shaftType: "Barite", level: null, auto: null }
+      ],
+      "Graveyard": []
+    }
+  },
+
+  Legacy: {}
+};
