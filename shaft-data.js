c13e161cac092f6107c80d488f300dfc740d101d
CheezeMasterGuru <cheezemasterguru@gmail.com>
2026-03-31 03:06:28 -0400

    Update shaft-data.js

--- shaft-data.js
+++ shaft-data.js
@@ -1,4 +1,5 @@
 window.ZM_SHAFT_DATA = {
+
   MainDeep: {
     "DEEP 1": {
       "Chamber 1": [],
@@ -45,179 +46,119 @@ window.ZM_SHAFT_DATA = {
         { shaftType: "Aventurine", level: 31, auto: 7 }
       ],
       "Graveyard": []
-    },
-
-    "Treasure Trove of Gems": {
-      "Chamber 1": [
-        { shaftType: "Beryl", level: 3, auto: 3 },
-        { shaftType: "Quartz", level: 7, auto: 4 }
-      ],
-      "Chamber 2": [
-        { shaftType: "Cobalt", level: 9, auto: 5 },
-        { shaftType: "Amber", level: 11, auto: 5 }
-      ],
-      "Chamber 3": [
-        { shaftType: "Ametrine", level: 13, auto: 7 },
-        { shaftType: "Barite", level: 15, auto: 7 }
-      ],
-      "Graveyard": []
-    },
-
-    "Essence Cave": {
-      "Chamber 1": [
-        { shaftType: "Beryl", level: 3, auto: 3 },
-        { shaftType: "Quartz", level: 6, auto: 4 }
-      ],
-      "Chamber 2": [
-        { shaftType: "Cobalt", level: 8, auto: 5 },
-        { shaftType: "Amber", level: 11, auto: 4 },
-        { shaftType: "Ametrine", level: 13, auto: 5 }
-      ],
-      "Chamber 3": [
-        { shaftType: "Barite", level: 16, auto: 6 }
-      ],
-      "Graveyard": []
-    },
-
-    "Excavations in the Sand": {
-      "Chamber 1": [
-        { shaftType: "Beryl", level: 3, auto: 3 },
-        { shaftType: "Quartz", level: 5, auto: 4 },
-        { shaftType: "Cobalt", level: 8, auto: 5 }
-      ],
-      "Chamber 2": [
-        { shaftType: "Amber", level: 8, auto: 5 },
-        { shaftType: "Ametrine", level: 16, auto: 6 },
-        { shaftType: "Barite", level: 18, auto: 6 }
-      ],
-      "Chamber 3": [
-        { shaftType: "Elendir", level: 12, auto: 6 },
-        { shaftType: "Lapis Lazuli", level: 21, auto: 7 },
-        { shaftType: "Aventurine", level: 23, auto: 7 }
-      ],
-      "Chamber 4": [
-        { shaftType: "Obsidian", level: 27, auto: 7 }
-      ],
-      "Graveyard": []
-    },
-
-    "Sweet Valley": {
-      "Chamber 1": [
-        { shaftType: "Beryl", level: 2, auto: 3 },
-        { shaftType: "Quartz", level: 4, auto: 3 },
-        { shaftType: "Cobalt", level: 6, auto: 4 }
-      ],
-      "Chamber 2": [
-        { shaftType: "Amber", level: 7, auto: 5 },
-        { shaftType: "Ametrine", level: 13, auto: 5 },
-        { shaftType: "Barite", level: 16, auto: 6 }
-      ],
-      "Chamber 3": [
-        { shaftType: "Elendir", level: 18, auto: 5 },
-        { shaftType: "Lapis Lazuli", level: 24, auto: 6 },
-        { shaftType: "Aventurine", level: 26, auto: 6 }
-      ],
-      "Chamber 4": [
-        { shaftType: "Obsidian", level: 20, auto: 7 }
-      ],
-      "Graveyard": []
-    },
-
-    "Grand Canyon": {
-      "Chamber 1": [
-        { shaftType: "Beryl", level: 3, auto: 3 },
-        { shaftType: "Quartz", level: 7, auto: 4 }
-      ],
-      "Chamber 2": [
-        { shaftType: "Cobalt", level: 8, auto: 5 },
-        { shaftType: "Amber", level: 14, auto: 5 },
-        { shaftType: "Ametrine", level: 19, auto: 6 }
-      ],
-      "Chamber 3": [
-        { shaftType: "Barite", level: 25, auto: 5 },
-        { shaftType: "Elendir", level: 27, auto: 6 },
-        { shaftType: "Lapis Lazuli", level: 29, auto: 7 }
-      ],
-      "Chamber 4": [
-        { shaftType: "Aventurine", level: 30, auto: 7 }
-      ],
-      "Graveyard": []
-    },
-
-    "Volcano Awakening": {
-      "Chamber 1": [
-        { shaftType: "Beryl", level: 3, auto: 3 },
-        { shaftType: "Quartz", level: 7, auto: 4 }
-      ],
-      "Chamber 2": [
-        { shaftType: "Cobalt", level: 8, auto: 5 },
-        { shaftType: "Amber", level: 14, auto: 5 },
-        { shaftType: "Ametrine", level: 19, auto: 6 }
-      ],
-      "Chamber 3": [
-        { shaftType: "Barite", level: 23, auto: 5 },
-        { shaftType: "Elendir", level: 26, auto: 6 },
-        { shaftType: "Lapis Lazuli", level: 28, auto: 7 }
-      ],
-      "Chamber 4": [
-        { shaftType: "Aventurine", level: 29, auto: 7 }
-      ],
-      "Graveyard": []
-    },
-
-    "Moon Odyssey": {
-      "Chamber 1": [
-        { shaftType: "Beryl", level: 2, auto: 3 },
-        { shaftType: "Quartz", level: 4, auto: 3 },
-        { shaftType: "Cobalt", level: 6, auto: 4 }
-      ],
-      "Chamber 2": [
-        { shaftType: "Amber", level: 7, auto: 5 },
-        { shaftType: "Ametrine", level: 13, auto: 5 },
-        { shaftType: "Barite", level: 18, auto: 6 }
-      ],
-      "Chamber 3": [
-        { shaftType: "Elendir", level: 18, auto: 5 },
-        { shaftType: "Lapis Lazuli", level: 26, auto: 6 },
-        { shaftType: "Aventurine", level: 26, auto: 6 }
-      ],
-      "Chamber 4": [
-        { shaftType: "Obsidian", level: 20, auto: 7 }
-      ],
-      "Graveyard": []
     }
 
   },
 
   "Legacy": {
-  "Easter Egg Hunt": {
 
-    "Mine 1": {
-      "Chamber 1": [
-        { shaftType: "Beryl", level: 3, auto: 3 },
-        { shaftType: "Quartz", level: 6, auto: 4 }
-      ],
-      "Chamber 2": [
-        { shaftType: "Cobalt", level: 5, auto: 4 },
-        { shaftType: "Amber", level: 7, auto: 3 }
-      ]
+    "Love Story": {
+      "Mine 1": {},
+      "Mine 2": {},
+      "Mine 3": {},
+      "Mine 4": {},
+      "Mine 5": {},
+      "The Deep": {}
     },
 
-    "Mine 2": {
-      "Chamber 1": [
-        { shaftType: "Beryl", level: 4, auto: 5 },
-        { shaftType: "Quartz", level: 8, auto: 5 }
-      ],
-      "Chamber 2": [
-        { shaftType: "Cobalt", level: 6, auto: 7 },
-        { shaftType: "Amber", level: 10, auto: 7 },
-        { shaftType: "Ametrine", level: 11, auto: 6 }
-      ],
-      "Chamber 3": [
-        { shaftType: "Barite", level: 9, auto: 5 }
-      ]
+    "Clover Valley": {
+      "Mine 1": {},
+      "Mine 2": {},
+      "Mine 3": {},
+      "Mine 4": {},
+      "Mine 5": {},
+      "The Deep": {}
+    },
+
+    "Easter Egg Hunt": {
+
+      "Mine 1": {
+        "Chamber 1": [
+          { shaftType: "Beryl", level: 3, auto: 3 },
+          { shaftType: "Quartz", level: 6, auto: 4 }
+        ],
+        "Chamber 2": [
+          { shaftType: "Cobalt", level: 5, auto: 4 },
+          { shaftType: "Amber", level: 7, auto: 3 }
+        ]
+      },
+
+      "Mine 2": {
+        "Chamber 1": [
+          { shaftType: "Beryl", level: 4, auto: 5 },
+          { shaftType: "Quartz", level: 8, auto: 5 }
+        ],
+        "Chamber 2": [
+          { shaftType: "Cobalt", level: 6, auto: 7 },
+          { shaftType: "Amber", level: 10, auto: 7 },
+          { shaftType: "Ametrine", level: 11, auto: 6 }
+        ],
+        "Chamber 3": [
+          { shaftType: "Barite", level: 9, auto: 5 }
+        ]
+      },
+
+      "Mine 3": {},
+      "Mine 4": {},
+      "Mine 5": {},
+      "The Deep": {}
+
+    },
+
+    "4th of July": {
+      "Mine 1": {},
+      "Mine 2": {},
+      "Mine 3": {},
+      "Mine 4": {},
+      "Mine 5": {},
+      "The Deep": {}
+    },
+
+    "Mystery Reef": {
+      "Mine 1": {},
+      "Mine 2": {},
+      "Mine 3": {},
+      "Mine 4": {},
+      "Mine 5": {},
+      "The Deep": {}
+    },
+
+    "Teamwork Festival": {
+      "Mine 1": {},
+      "Mine 2": {},
+      "Mine 3": {},
+      "Mine 4": {},
+      "Mine 5": {},
+      "The Deep": {}
+    },
+
+    "Halloween": {
+      "Mine 1": {},
+      "Mine 2": {},
+      "Mine 3": {},
+      "Mine 4": {},
+      "Mine 5": {},
+      "The Deep": {}
+    },
+
+    "Fall Festival": {
+      "Mine 1": {},
+      "Mine 2": {},
+      "Mine 3": {},
+      "Mine 4": {},
+      "Mine 5": {},
+      "The Deep": {}
+    },
+
+    "Winter Break": {
+      "Mine 1": {},
+      "Mine 2": {},
+      "Mine 3": {},
+      "Mine 4": {},
+      "Mine 5": {},
+      "The Deep": {}
     }
 
   }
-}
+
 };
