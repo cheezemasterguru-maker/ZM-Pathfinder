5b4462b7d8d64c22f4a1795603103cb6e3836c84
CheezeMasterGuru <cheezemasterguru@gmail.com>
2026-04-04 01:38:00 -0400

    Update shaft-data.js

--- shaft-data.js
+++ shaft-data.js
@@ -1,5 +1,4 @@
 window.ZM_SHAFT_DATA = {
-
   MainDeep: {
     "DEEP 1": {
       "Chamber 1": [],
@@ -26,7 +25,6 @@ window.ZM_SHAFT_DATA = {
   },
 
   Main: {
-
     "Treasures in Ice": {
       "Chamber 1": [
         { shaftType: "Beryl", level: 3, auto: 3 },
@@ -47,11 +45,9 @@ window.ZM_SHAFT_DATA = {
       ],
       "Graveyard": []
     }
-
   },
 
-  "Legacy": {
-
+  Legacy: {
     "Love Story": {
       "Mine 1": {},
       "Mine 2": {},
@@ -71,7 +67,6 @@ window.ZM_SHAFT_DATA = {
     },
 
     "Easter Egg Hunt": {
-
       "Mine 1": {
         "Chamber 1": [
           { shaftType: "Beryl", level: 3, auto: 3 },
@@ -99,122 +94,112 @@ window.ZM_SHAFT_DATA = {
       },
 
       "Mine 3": {
-  "Chamber 1": [
-    { shaftType: "Beryl", level: 5, auto: 7 },
-    { shaftType: "Quartz", level: 7, auto: 7 },
-    { shaftType: "Cobalt", level: 10, auto: 8 }
-  ],
-  "Chamber 2": [
-    { shaftType: "Amber", level: 8, auto: 8 },
-    { shaftType: "Ametrine", level: 13, auto: 7 },
-    { shaftType: "Barite", level: 15, auto: 8 }
-  ],
-  "Chamber 3": [
-    { shaftType: "Elendir", level: 13, auto: 7 },
-    { shaftType: "Lapis Lazuli", level: 18, auto: 6 }
-  ]
-},
-      
+        "Chamber 1": [
+          { shaftType: "Beryl", level: 5, auto: 7 },
+          { shaftType: "Quartz", level: 7, auto: 7 },
+          { shaftType: "Cobalt", level: 10, auto: 8 }
+        ],
+        "Chamber 2": [
+          { shaftType: "Amber", level: 8, auto: 8 },
+          { shaftType: "Ametrine", level: 13, auto: 7 },
+          { shaftType: "Barite", level: 15, auto: 8 }
+        ],
+        "Chamber 3": [
+          { shaftType: "Elendir", level: 13, auto: 7 },
+          { shaftType: "Lapis Lazuli", level: 18, auto: 6 }
+        ]
+      },
+
       "Mine 4": {
-  "Chamber 1": [
-    { shaftType: "Beryl", level: 8, auto: 8 },
-    { shaftType: "Quartz", level: 11, auto: 8 },
-    { shaftType: "Cobalt", level: 13, auto: 9 }
-  ],
-  "Chamber 2": [
-    { shaftType: "Amber", level: 14, auto: 9 },
-    { shaftType: "Ametrine", level: 19, auto: 10 }
-  ],
-  "Chamber 3": [
-    { shaftType: "Barite", level: 17, auto: 9 },
-    { shaftType: "Elendir", level: 21, auto: 9 },
-    { shaftType: "Lapis Lazuli", level: 23, auto: 9 }
-  ],
-  "Chamber 4": [
-    { shaftType: "Aventurine", level: 23, auto: 8 },
-    { shaftType: "Obsidian", level: 27, auto: 7 }
-  ]
-},
-      
+        "Chamber 1": [
+          { shaftType: "Beryl", level: 8, auto: 8 },
+          { shaftType: "Quartz", level: 11, auto: 8 },
+          { shaftType: "Cobalt", level: 13, auto: 9 }
+        ],
+        "Chamber 2": [
+          { shaftType: "Amber", level: 14, auto: 9 },
+          { shaftType: "Ametrine", level: 19, auto: 10 }
+        ],
+        "Chamber 3": [
+          { shaftType: "Barite", level: 17, auto: 9 },
+          { shaftType: "Elendir", level: 21, auto: 9 },
+          { shaftType: "Lapis Lazuli", level: 23, auto: 9 }
+        ],
+        "Chamber 4": [
+          { shaftType: "Aventurine", level: 23, auto: 8 },
+          { shaftType: "Obsidian", level: 27, auto: 7 }
+        ]
+      },
+
       "Mine 5": {
-  "Chamber 1": [
-    { shaftType: "Beryl", level: 11, auto: 10 },
-    { shaftType: "Quartz", level: 17, auto: 10 },
-    { shaftType: "Cobalt", level: 20, auto: 11 }
-  ],
+        "Chamber 1": [
+          { shaftType: "Beryl", level: 11, auto: 10 },
+          { shaftType: "Quartz", level: 17, auto: 10 },
+          { shaftType: "Cobalt", level: 20, auto: 11 }
+        ],
+        "Chamber 2": [
+          { shaftType: "Amber", level: 16, auto: 10 },
+          { shaftType: "Ametrine", level: 23, auto: 11 },
+          { shaftType: "Barite", level: 25, auto: 10 }
+        ],
+        "Chamber 3": [
+          { shaftType: "Elendir", level: 23, auto: 10 },
+          { shaftType: "Lapis Lazuli", level: 26, auto: 10 },
+          { shaftType: "Aventurine", level: 30, auto: 10 }
+        ],
+        "Chamber 4": [
+          { shaftType: "Obsidian", level: 34, auto: 9 },
+          { shaftType: "Peridot", level: 37, auto: 9 }
+        ]
+      },
 
-  "Chamber 2": [
-    { shaftType: "Amber", level: 16, auto: 10 },
-    { shaftType: "Ametrine", level: 23, auto: 11 },
-    { shaftType: "Barite", level: 25, auto: 10 }
-  ],
+      "Mine 6": {
+        "Chamber 1": [
+          { shaftType: "Beryl", level: 10, auto: 11 },
+          { shaftType: "Quartz", level: 22, auto: 11 }
+        ],
+        "Chamber 2": [
+          { shaftType: "Cobalt", level: 23, auto: 12 },
+          { shaftType: "Amber", level: 26, auto: 12 },
+          { shaftType: "Ametrine", level: 28, auto: 12 }
+        ],
+        "Chamber 3": [
+          { shaftType: "Barite", level: 33, auto: 12 },
+          { shaftType: "Elendir", level: 35, auto: 12 },
+          { shaftType: "Lapis Lazuli", level: 37, auto: 12 }
+        ],
+        "Chamber 4": [
+          { shaftType: "Aventurine", level: 39, auto: 12 },
+          { shaftType: "Obsidian", level: 43, auto: 12 },
+          { shaftType: "Peridot", level: 47, auto: 12 }
+        ],
+        "Graveyard": []
+      },
 
-  "Chamber 3": [
-    { shaftType: "Elendir", level: 23, auto: 10 },
-    { shaftType: "Lapis Lazuli", level: 26, auto: 10 },
-    { shaftType: "Aventurine", level: 30, auto: 10 }
-  ],
-
-  "Chamber 4": [
-    { shaftType: "Obsidian", level: 34, auto: 9 },
-    { shaftType: "Peridot", level: 37, auto: 9 }
-  ]
-},
-
-"Mine 6": {
-  "Chamber 1": [
-    { shaftType: "Beryl", level: 10, auto: 11 },
-    { shaftType: "Quartz", level: 22, auto: 11 }
-  ],
-
-  "Chamber 2": [
-    { shaftType: "Cobalt", level: 23, auto: 12 },
-    { shaftType: "Amber", level: 26, auto: 12 },
-    { shaftType: "Ametrine", level: 28, auto: 12 }
-  ],
-
-  "Chamber 3": [
-    { shaftType: "Barite", level: 33, auto: 12 },
-    { shaftType: "Elendir", level: 35, auto: 12 },
-    { shaftType: "Lapis Lazuli", level: 37, auto: 12 }
-  ],
-
-  "Chamber 4": [
-    { shaftType: "Aventurine", level: 39, auto: 12 },
-    { shaftType: "Obsidian", level: 43, auto: 12 },
-    { shaftType: "Peridot", level: 47, auto: 12 }
-  ],
-
-  "Graveyard": []
-},
       "The Deep": {
-  "Chamber 1": [
-    { shaftType: "Beryl", level: 9, auto: 12 },
-    { shaftType: "Quartz", level: 21, auto: 12 }
-  ],
-
-  "Chamber 2": [
-    { shaftType: "Cobalt", level: 22, auto: 13 },
-    { shaftType: "Amber", level: 25, auto: 13 },
-    { shaftType: "Ametrine", level: 28, auto: 13 }
-  ],
-
-  "Chamber 3": [
-    { shaftType: "Barite", level: 32, auto: 13 },
-    { shaftType: "Elendir", level: 34, auto: 13 },
-    { shaftType: "Lapis Lazuli", level: 37, auto: 13 }
-  ],
-
-  "Chamber 4": [
-    { shaftType: "Aventurine", level: 41, auto: 13 },
-    { shaftType: "Obsidian", level: 44, auto: 13 }
-  ],
-
-  "Graveyard": [
-    { shaftType: "Peridot", level: 55, auto: 13 }
-  ]
-}        
-},
+        "Chamber 1": [
+          { shaftType: "Beryl", level: 9, auto: 12 },
+          { shaftType: "Quartz", level: 21, auto: 12 }
+        ],
+        "Chamber 2": [
+          { shaftType: "Cobalt", level: 22, auto: 13 },
+          { shaftType: "Amber", level: 25, auto: 13 },
+          { shaftType: "Ametrine", level: 28, auto: 13 }
+        ],
+        "Chamber 3": [
+          { shaftType: "Barite", level: 32, auto: 13 },
+          { shaftType: "Elendir", level: 34, auto: 13 },
+          { shaftType: "Lapis Lazuli", level: 37, auto: 13 }
+        ],
+        "Chamber 4": [
+          { shaftType: "Aventurine", level: 41, auto: 13 },
+          { shaftType: "Obsidian", level: 44, auto: 13 }
+        ],
+        "Graveyard": [
+          { shaftType: "Peridot", level: 55, auto: 13 }
+        ]
+      }
+    },
 
     "4th of July": {
       "Mine 1": {},
@@ -249,6 +234,7 @@ window.ZM_SHAFT_DATA = {
       "Mine 3": {},
       "Mine 4": {},
       "Mine 5": {},
+      "Mine 6": {},
       "The Deep": {}
     },
 
@@ -258,6 +244,7 @@ window.ZM_SHAFT_DATA = {
       "Mine 3": {},
       "Mine 4": {},
       "Mine 5": {},
+      "Mine 6": {},
       "The Deep": {}
     },
 
@@ -267,9 +254,8 @@ window.ZM_SHAFT_DATA = {
       "Mine 3": {},
       "Mine 4": {},
       "Mine 5": {},
+      "Mine 6": {},
       "The Deep": {}
     }
-
   }
-
 };
