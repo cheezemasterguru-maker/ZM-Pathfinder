37c6164007cb8c11cf8f65dd9bfc73e849ad8f47
CheezeMasterGuru <cheezemasterguru@gmail.com>
2026-04-06 08:43:56 -0400

    Update tile-meta.js

--- tile-meta.js
+++ tile-meta.js
@@ -994,8 +994,8 @@ window.ZM_TILE_META = {
         "16,1": { object: "essence" },  
         "16,4": { object: "essence" }  
       }  
-    }  
-  ,  
+    }, 
+      
   "The Deep": {
       "Chamber 1": {
         tiles: {
