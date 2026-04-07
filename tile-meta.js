519b7087d8bdda3bf68feb71fd8a57883629bdf3
CheezeMasterGuru <cheezemasterguru@gmail.com>
2026-04-07 05:58:58 -0400

    Update tile-meta.js

--- tile-meta.js
+++ tile-meta.js
@@ -1,16 +1,155 @@
 window.ZM_TILE_META = {
   MainDeep: {
+
     "DEEP 1": {
-      "Chamber 1": { tiles: {} },
-      "Chamber 2": { tiles: {} },
-      "Chamber 3": { tiles: {} }
+
+      "Chamber 1": {
+        tiles: {
+          "0,0": { object: "stickers" },
+          "0,1": { object: "chest", subtype: "steel" },
+          "0,3": { object: "essence" },
+          "0,4": { object: "badges" },
+          "1,2": { object: "chest", subtype: "wood" },
+          "1,6": { object: "badges" },
+          "2,3": { object: "badges" },
+          "3,4": { object: "essence" },
+          "4,1": { object: "gems" },
+          "4,3": { object: "gems" },
+          "5,0": { object: "essence" },
+          "5,2": { object: "essence" },
+          "5,4": { object: "badges" },
+          "7,1": { object: "badges" },
+          "8,4": { object: "chest", subtype: "wood" },
+          "9,3": { object: "chest", subtype: "wood" }
+        }
+      },
+
+      "Chamber 2": {
+        tiles: {
+          "0,1": { object: "badges" },
+          "0,3": { object: "gems" },
+          "0,5": { object: "badges" },
+          "1,6": { object: "essence" },
+          "2,2": { object: "chest", subtype: "steel" },
+          "3,4": { object: "chest", subtype: "wood" },
+          "4,1": { object: "essence" },
+          "4,2": { object: "gems" },
+          "5,0": { object: "badges" },
+          "5,1": { object: "chest", subtype: "wood" },
+          "5,3": { object: "chest", subtype: "steel" },
+          "5,4": { object: "badges" },
+          "6,3": { object: "essence" },
+          "7,4": { object: "gems" },
+          "8,2": { object: "essence" },
+          "9,3": { object: "essence" }
+        }
+      },
+
+      "Chamber 3": {
+        tiles: {
+          "0,0": { object: "stickers" },
+          "0,4": { object: "essence" },
+          "0,6": { object: "chest", subtype: "steel" },
+          "1,1": { object: "gems" },
+          "1,3": { object: "badges" },
+          "2,4": { object: "essence" },
+          "3,3": { object: "chest", subtype: "wood" },
+          "4,4": { object: "chest", subtype: "steel" },
+          "5,3": { object: "gems" },
+          "6,2": { object: "badges" },
+          "7,4": { object: "essence" },
+          "8,5": { object: "essence" },
+          "10,3": { object: "badges" }
+        }
+      }
     },
 
     "DEEP 2": {
-      "Chamber 1": { tiles: {} },
-      "Chamber 2": { tiles: {} },
-      "Chamber 3": { tiles: {} },
-      "Chamber 4": { tiles: {} }
+
+      "Chamber 1": {
+        tiles: {
+          "0,2": { object: "badges" },
+          "0,4": { object: "gems" },
+          "1,0": { object: "stickers" },
+          "1,1": { object: "chest", subtype: "wood" },
+          "1,3": { object: "essence" },
+          "1,5": { object: "chest", subtype: "wood" },
+          "2,2": { object: "badges" },
+          "3,6": { object: "essence" },
+          "4,2": { object: "chest", subtype: "wood" },
+          "4,5": { object: "essence" },
+          "5,1": { object: "badges" },
+          "6,2": { object: "gems" },
+          "6,4": { object: "gems" },
+          "7,3": { object: "essence" },
+          "7,4": { object: "essence" },
+          "8,3": { object: "badges" }
+        }
+      },
+
+      "Chamber 2": {
+        tiles: {
+          "0,1": { object: "essence" },
+          "0,5": { object: "essence" },
+          "1,2": { object: "badges" },
+          "1,4": { object: "badges" },
+          "2,3": { object: "gems" },
+          "3,4": { object: "essence" },
+          "3,5": { object: "chest", subtype: "steel" },
+          "4,3": { object: "essence" },
+          "5,0": { object: "stickers" },
+          "5,2": { object: "essence" },
+          "6,1": { object: "badges" },
+          "6,3": { object: "gems" },
+          "8,3": { object: "chest", subtype: "wood" },
+          "9,2": { object: "badges" },
+          "9,6": { object: "chest", subtype: "wood" },
+          "10,4": { object: "gems" }
+        }
+      },
+
+      "Chamber 3": {
+        tiles: {
+          "0,2": { object: "badges" },
+          "0,5": { object: "badges" },
+          "1,3": { object: "gems" },
+          "2,3": { object: "essence" },
+          "3,2": { object: "gems" },
+          "4,3": { object: "chest", subtype: "steel" },
+          "5,2": { object: "essence" },
+          "5,4": { object: "badges" },
+          "5,5": { object: "essence" },
+          "6,3": { object: "gems" },
+          "7,4": { object: "badges" },
+          "9,3": { object: "badges" },
+          "10,2": { object: "chest", subtype: "iron" },
+          "11,0": { object: "chest", subtype: "wood" },
+          "11,3": { object: "chest", subtype: "wood" },
+          "12,6": { object: "essence" }
+        }
+      },
+
+      "Chamber 4": {
+        tiles: {
+          "0,1": { object: "essence" },
+          "0,3": { object: "badges" },
+          "0,4": { object: "badges" },
+          "0,6": { object: "essence" },
+          "1,1": { object: "chest", subtype: "steel" },
+          "1,6": { object: "chest", subtype: "silver" },
+          "2,3": { object: "gems" },
+          "3,2": { object: "gems" },
+          "3,3": { object: "chest", subtype: "iron" },
+          "3,4": { object: "gems" },
+          "5,2": { object: "badges" },
+          "5,3": { object: "essence" },
+          "6,4": { object: "chest", subtype: "iron" },
+          "7,2": { object: "essence" },
+          "8,3": { object: "essence" },
+          "9,5": { object: "badges" },
+          "10,2": { object: "badges" },
+          "10,3": { object: "chest", subtype: "wood" },
+          "10,6": { object: "badges" }
     },
 
     "DEEP 3": {
