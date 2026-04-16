05fecd4dce2d8d272d343af20198c9cdb40d8dbe
CheezeMasterGuru <cheezemasterguru@gmail.com>
2026-04-16 14:11:46 -0400

    Update tile-meta.js

--- tile-meta.js
+++ tile-meta.js
@@ -451,10 +451,112 @@ window.ZM_TILE_META = {
     },
 
     "Sweet Valley": {
-      "Chamber 1": { tiles: {} },
-      "Chamber 2": { tiles: {} },
-      "Chamber 3": { tiles: {} },
-      "Chamber 4": { tiles: {} },
+  "Chamber 1": {
+    tiles: {
+      "0,1": { object: "chest", subtype: "iron" },
+      "0,4": { object: "emblems" },
+      "0,6": { object: "keys" },
+      "1,0": { object: "keys" },
+      "1,5": { object: "essence" },
+      "2,1": { object: "essence" },
+      "3,1": { object: "emblems" },
+      "3,3": { object: "chest", subtype: "wood" },
+      "4,1": { object: "chest", subtype: "wood" },
+      "4,2": { object: "keys" },
+      "5,6": { object: "essence" },
+      "6,3": { object: "chest", subtype: "iron" },
+      "6,5": { object: "keys" },
+      "9,0": { object: "emblems" },
+      "9,1": { object: "essence" },
+      "9,3": { object: "chest", subtype: "wood" },
+      "10,2": { object: "keys" }
+    }
+  },
+
+  "Chamber 2": {
+    tiles: {
+      "0,2": { object: "essence" },
+      "0,3": { object: "emblems" },
+      "0,5": { object: "keys" },
+      "0,6": { object: "emblems" },
+      "1,1": { object: "chest", subtype: "steel" },
+      "1,4": { object: "essence" },
+      "1,6": { object: "chest", subtype: "wood" },
+      "2,0": { object: "keys" },
+      "2,1": { object: "emblems" },
+      "2,5": { object: "chest", subtype: "iron" },
+      "3,3": { object: "keys" },
+      "4,2": { object: "essence" },
+      "5,3": { object: "chest", subtype: "wood" },
+      "6,5": { object: "emblems" },
+      "6,6": { object: "keys" },
+      "7,4": { object: "essence" },
+      "8,1": { object: "emblems" },
+      "8,2": { object: "keys" },
+      "9,1": { object: "chest", subtype: "wood" },
+      "9,3": { object: "chest", subtype: "wood" }
+    }
+  },
+
+  "Chamber 3": {
+    tiles: {
+      "0,0": { object: "essence" },
+      "0,2": { object: "emblems" },
+      "0,5": { object: "essence" },
+      "1,1": { object: "chest", subtype: "steel" },
+      "1,3": { object: "keys" },
+      "2,0": { object: "keys" },
+      "3,3": { object: "essence" },
+      "4,5": { object: "chest", subtype: "wood" },
+      "5,3": { object: "chest", subtype: "iron" },
+      "5,6": { object: "emblems" },
+      "6,2": { object: "emblems" },
+      "6,5": { object: "keys" },
+      "6,6": { object: "chest", subtype: "wood" },
+      "7,1": { object: "keys" },
+      "7,3": { object: "essence" },
+      "8,2": { object: "chest", subtype: "wood" },
+      "8,4": { object: "essence" },
+      "9,1": { object: "essence" },
+      "9,3": { object: "chest", subtype: "wood" },
+      "10,2": { object: "essence" },
+      "10,6": { object: "emblems" }
+    }
+  },
+
+  "Chamber 4": {
+    tiles: {
+      "0,0": { object: "keys" },
+      "0,1": { object: "essence" },
+      "0,3": { object: "emblems" },
+      "0,4": { object: "essence" },
+      "0,6": { object: "keys" },
+      "1,0": { object: "essence" },
+      "1,5": { object: "chest", subtype: "steel" },
+      "2,2": { object: "chest", subtype: "steel" },
+      "2,3": { object: "keys" },
+      "2,6": { object: "essence" },
+      "3,1": { object: "emblems" },
+      "3,4": { object: "emblems" },
+      "4,2": { object: "essence" },
+      "4,5": { object: "essence" },
+      "5,4": { object: "chest", subtype: "steel" },
+      "5,6": { object: "essence" },
+      "6,0": { object: "keys" },
+      "6,1": { object: "essence" },
+      "6,3": { object: "chest", subtype: "iron" },
+      "6,5": { object: "emblems" },
+      "7,0": { object: "chest", subtype: "steel" },
+      "7,4": { object: "essence" },
+      "8,2": { object: "keys" },
+      "9,3": { object: "chest", subtype: "steel" },
+      "9,6": { object: "keys" },
+      "10,4": { object: "essence" },
+      "10,6": { object: "emblems" },
+      "11,5": { object: "chest", subtype: "wood" }
+    }
+  },
+      
       "Graveyard": { tiles: {} }
     },
 
